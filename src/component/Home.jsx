9641fd940c5323ab22319ec4447aa757b0ad8792
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-05-13 16:10:47 +0100

    new

---
+++ src/component/Home.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+
+export default function Home() {
+    const customStyles = {
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+        },
+      };
+
+    let subtitle;
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+  
+    function openModal() {
+      setIsOpen(true);
+    }
+  
+    function afterOpenModal() {
+      // references are now sync'd and can be accessed.
+      subtitle.style.color = '#f00';
+    }
+  
+    function closeModal() {
+      setIsOpen(false);
+    }
+  return (
+    <div>
+        
+    <div >
+      <button className='bg-black text-white mt-20 ml-40 rounded-md w-[10%] pt-6 pb-6' onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>FACE UNDER CONSTRUCTION</div>
+        <p>😱😱😱😱😱😱😱😱😱😱😱😱😱😱😱😱 </p>
+
+
+        {/* <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form> */}
+      </Modal>
+    </div>
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
